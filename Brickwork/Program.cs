d2c0625a3b1694bfef185168ee44cdba4c62177a
IvanLefterov89 <ivanlefterov1@gmail.com>
2020-12-06 11:17:47 +0200

    Add project files.

---
+++ Brickwork/Program.cs
@@ -0,0 +1,253 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Brickwork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // defining the dimensions
+            int[] dim = Console.ReadLine()
+               .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+               .Select(int.Parse)
+               .ToArray();
+            // defining the area for the first layer
+            int[,] area = new int[dim[0], dim[1]];
+            // validating the dimensions
+            if (dim[0] %2 !=0 || dim[1] %2 != 0 || dim[0]>100 || dim[0]>100)
+            {
+                Console.WriteLine($"Dimensions need to be even number and less than 100");
+                return;
+            }
+            //filling the area with "bricks"
+            for (int i = 0; i < area.GetLength(0); i++)
+            {
+                int[] tokens = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+                for (int j = 0; j < area.GetLength(1); j++)
+                {
+                    area[i, j] = tokens[j];
+                }
+
+            }
+            //defining new array for the second layer
+            List<string> newArea = new List<string>();
+
+            if (dim[0] * dim[1] <= 4)                  
+            {
+                Console.WriteLine($"Area too small");
+            }
+            //looping the first layer to arrange the bricks for the second layer based on the requirements and validating solutions
+            else
+            {
+                for (int i = 0; i < area.GetLength(0); i+=2)
+                {
+                    for (int j = 0; j < area.GetLength(1); j+=4)
+                    {
+                        if (area.GetLength(1) - j > 2 && area[i,j] == area[i,j+1] && area[i,j+2] == area[i,j+3])
+                        {
+                            // 1 1 2 2 | 2 1 1 4
+                            // 3 3 4 4 | 2 3 3 4
+                            if (area[i+1,j] == area[i+1,j+1] && area[i+1,j+2] == area[i+1,j+3])
+                            {
+                                newArea.Add($"|{area[i,j+2]}||{area[i,j]}-{area[i,j+1]}||{area[i+1,j+3]}|");
+                                newArea.Add($"|{area[i,j+3]}||{area[i+1,j]}-{area[i+1,j+1]}||{area[i+1,j+2]}|");
+                            }
+                           else 
+                           { 
+                                Console.WriteLine("No solution exists!");
+                                return;
+                           } 
+                           
+                            
+                        }
+                        if (area.GetLength(1) - j > 2 && area[i,j] != area[i,j+1] && area[i,j+1] == area[i,j+2])
+                        {
+                            // 2 1 1 4 | 1 1 2 2
+                            // 2 3 3 4 | 3 3 4 4
+                            if (area[i,j] == area[i+1,j] && area[i+1,j+1] == area[i+1,j+2] && area[i,j+3] == area[i+1,j+3])
+                            {
+                                newArea.Add($"|{area[i,j+1]}-{area[i,j+2]}||{area[i,j]}-{area[i+1,j]}|");
+                                newArea.Add($"|{area[i+1,j+1]}-{area[i+1,j+2]}||{area[i,j+3]}-{area[i+1,j+3]}|");
+                            }
+                            else 
+                            { 
+                                Console.WriteLine("No solution exists!");
+                                return;
+                            } 
+                        }
+
+                        if (area.GetLength(1) - j > 2 && area[i,j] != area[i,j+1] && area[i,j+1] != area[i,j+2] && area[i,j+2] != area[i,j+3])
+                        {
+                            // 1 2 3 4 | 1 1 2 2
+                            // 1 2 3 4 | 3 3 4 4
+                            if (area[i,j] == area[i+1,j] && area[i,j+1] == area[i+1,j+1] && area[i,j+2] == area[i+1,j+2] && area[i,j+3] == area[i+1,j+3])
+                            {
+                                newArea.Add($"|{area[i, j]}-{area[i+1, j]}||{area[i, j+1]}-{area[i+1, j+1]}|");
+                                newArea.Add($"|{area[i, j+2]}-{area[i+1, j+2]}||{area[i, j+3]}-{area[i+1, j+3]}|");
+
+                            }
+                            else 
+                            { 
+                                Console.WriteLine("No solution exists!");
+                                return;
+                            } 
+                        }
+
+                        if (area.GetLength(1) - j > 2 && area[i,j] == area[i,j+1] && area[i,j+2] != area[i,j+3])
+                        {
+                            // 1 1 2 3 | 1 2 3 3
+                            // 4 4 2 3 | 1 2 4 4
+                            if (area[i+1,j] == area[i+1,j+1] && area[i,j+2] == area[i+1,j+2] && area[i,j+3] == area[i+1,j+3])
+                            {
+                                newArea.Add($"|{area[i, j]}||{area[i, j+2]}||{area[i, j+3]}-{area[i+1, j+3]}|");
+                                newArea.Add($"|{area[i, j+1]}||{area[i+1, j+2]}||{area[i+1, j]}-{area[i+1, j+1]}|");
+                            }
+
+                            else 
+                            { 
+                                Console.WriteLine("No solution exists!");
+                                return;
+                            } 
+                        }
+
+                        if (area.GetLength(1) - j > 2 && area[i,j] != area[i,j+1] && area[i,j+2] == area[i,j+3])
+                        {
+                            // 1 2 3 3 | 1 1 2 3
+                            // 1 2 4 4 | 4 4 2 3
+                            if (area[i,j] == area[i+1,j] && area[i,j+1] == area[i+1,j+1] && area[i+1,j+2] == area[i+1,j+3])
+                            {
+                                newArea.Add($"|{area[i,j]}-{area[i+1, j]}||{area[i, j+1]}||{area[i, j+2]}|");
+                                newArea.Add($"|{area[i+1, j+2]}-{area[i+1, j+3]}||{area[i+1, j+1]}||{area[i, j+3]}|");
+                            }
+                            
+                            else 
+                            { 
+                                Console.WriteLine("No solution exists!");
+                                return;
+                            }
+                        }
+
+                        if (area.GetLength(1) % 4 !=0 && area.GetLength(1) - j == 2)
+                        {
+                                if (area[i,j] == area[i,j+1] && area[i+1,j] == area[i+1,j+1])
+                                {
+                                    newArea.Add($"|{area[i,j]}|{area[i+1,j]}|");
+                                    newArea.Add($"|{area[i,j]}|{area[i+1,j]}|");
+                                    continue;
+                                }
+
+                                if (area[i,j] == area[i+1,j] && area[i,j+1] == area[i+1,j+1])
+                                { 
+                                    newArea.Add($"|{area[i,j]}-{area[i+1,j]}|");
+                                    newArea.Add($"|{area[i,j+1]}-{area[i+1,j+1]}|");
+                                    continue;
+                                }
+
+                                else
+                                { 
+                                    Console.WriteLine("No solution exists!");
+                                    return;
+                                }
+                        }
+                    }
+                  
+                }
+                Console.WriteLine("============================");
+                //generating the output 
+               int count = 0;
+                    if (area.GetLength(0) == 2 && area.GetLength(1)>4)
+                    {
+                        for (count = 0; count < area.GetLength(1) /4; count++)
+                        {
+                            if (area.GetLength(1) % 4 !=0 && area.GetLength(1) - (count + 1) * 4 == 2)
+                            {
+                                Console.Write($"{newArea[count *2]}{newArea[count *2+2]}");
+                            }
+                           else  Console.Write($"{newArea[count *2]}");
+                        }
+                        Console.WriteLine();
+
+                        for (count = 1; count <= area.GetLength(1) /4; count++)
+                        {
+                            if (area.GetLength(1) % 4 !=0 && area.GetLength(1) - count * 4 == 2)
+                            {
+                                Console.Write($"{newArea[count *2 -1]}{newArea[count *2+1]}");
+                            }
+                            else Console.Write($"{newArea[count * 2 -1]}");
+                        }
+                        
+                    }
+                    if (area.GetLength(0) > 2 && area.GetLength(1) == 4)
+                    {
+                        for (count = 0; count < area.GetLength(0); count++)
+                        {
+                            Console.Write($"{newArea[count]}\n");
+                        }
+                    }
+                    if (area.GetLength(0) > 2 && area.GetLength(1)>4)
+                    {
+                        for (int i = 0; i < area.GetLength(0)/2; i++)
+                        {
+                            if (i>0)
+                            {
+                                for (count = i*2; count < area.GetLength(1) /2*i; count++)
+                                {
+                                    if (area.GetLength(1) % 4 !=0 && area.GetLength(1) - (count - 1) * 4 == 2)
+                                    {
+                                        Console.Write($"{newArea[count *2]}{newArea[count *2+2]}");
+                                    }
+                                    else Console.Write($"{newArea[count *2]}");
+                                }
+                                Console.WriteLine();
+
+                                for (count = i*2+1; count <= area.GetLength(1) /2*i; count++)
+                                {   
+                                    if (area.GetLength(1) % 4 !=0 && area.GetLength(1) - (count - 2)* 4 == 2)
+                                    {
+                                        Console.Write($"{newArea[count *2 -1]}{newArea[count *2+1]}");
+                                    }
+                                    else Console.Write($"{newArea[count * 2 -1]}");
+                                }
+                                Console.WriteLine();
+                            }
+                            else 
+                            { 
+                                for (count = 0; count < area.GetLength(1) /4; count++)
+                                {   
+                                    if (area.GetLength(1) % 4 !=0 && area.GetLength(1) - (count + 1)* 4 == 2)
+                                    {
+                                        Console.Write($"{newArea[count *2]}{newArea[count *2+2]}");
+                                    }
+                                    else  Console.Write($"{newArea[count *2]}");
+                                }
+                                Console.WriteLine();
+
+                                for (count = 1; count <= area.GetLength(1) /4; count++)
+                                {
+                                    if (area.GetLength(1) % 4 !=0 && area.GetLength(1) - count * 4 == 2)
+                                    {
+                                        Console.Write($"{newArea[count *2 -1]}{newArea[count *2+1]}");
+                                    }
+                                    else Console.Write($"{newArea[count * 2 -1]}");
+                                }
+                                Console.WriteLine();
+                            }
+                        }
+                    }
+                    if(area.GetLength(0) == 2 && area.GetLength(1) == 4)
+                    { 
+                        Console.Write($"{newArea[count]}\n");
+                        Console.Write($"{newArea[count+1]}");
+                   
+                    } 
+               
+            }
+        }
+    }
+}
